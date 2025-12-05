3b84bdb7677030d72d7fe9df7948786deb4b0f83
Tarun-Upadhyay-616 <upadhyaytarun616@gmail.com>
2025-12-05 20:26:11 +0530

    basic server setup env config , db connection , usermodel and imageArtifact schemas

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,4 +1,30 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt"
 const userSchema = new Schema({
-    username
+    username:{
+        type: String,
+        required: [true,"Username is required"],
+        unique: true,
+        minlength: 3,
+    },
+    email:{
+        type: String,
+        required: [true,"Email is required"],
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
+    password:{
+        type: String,
+        required: [true,"Password is required"],
+        minlength: 8,
+    },
+    credits:{
+        type: Number,
+        default: 10,
+        min: 0,
+    },
 })
+const hashedPassword = bcrypt.hash(password,10)
+this.password = hashedPassword
+export const User = mongoose.model('User',userSchema)
